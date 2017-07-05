51c343c0c8695c00224d1b00849e96277e934dd6
PanAeon <vitalii.voloshyn@gmail.com>
2017-07-05 23:50:52 +0300

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL

--- app/Main.hs
+++ app/Main.hs
@@ -4,5 +4,7 @@ import Lib
 -- https://haskell-lang.org/library/http-client
 -- https://hackage.haskell.org/package/hs-duktape
 -- https://hackage.haskell.org/package/jsaddle
+-- curl -s whatthecommit.com |  grep -A2 '<div id="content">' |  tail -n +2 | head -n +1 | sed 's/<p>//'
+
 main :: IO ()
 main = someFunc
