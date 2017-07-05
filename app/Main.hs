48cbe9afaf5d5f2dc9d48d03dc633c2bb567d7d1
PanAeon <vitalii.voloshyn@gmail.com>
2017-07-06 00:00:21 +0300

    Nothing to see here, move along

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Lib
 -- https://hackage.haskell.org/package/hs-duktape
 -- https://hackage.haskell.org/package/jsaddle
 -- curl -s whatthecommit.com |  grep -A2 '<div id="content">' |  tail -n +2 | head -n +1 | sed 's/<p>//'
+-- xinput --set-prop "FTE1001:00 0B05:0101" 'Device Enabled' $(xinput --list-props 'FTE1001:00 0B05:0101' | grep -c 'Device Enabled (139):[[:space:]].*0')
 
 main :: IO ()
 main = someFunc
