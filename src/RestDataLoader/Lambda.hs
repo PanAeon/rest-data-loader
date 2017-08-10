9988f5fe6a0628295e84f98a94045838bf8b98dd
PanAeon <vitalii.voloshyn@gmail.com>
2017-08-10 11:16:43 +0300

    "Get that shit outta my master."

--- src/RestDataLoader/Lambda.hs
+++ src/RestDataLoader/Lambda.hs
@@ -9,8 +9,9 @@ import Text.Parsec.Combinator (many1, chainl1, between, eof, optionMaybe)
 import Control.Applicative ((<$>), (<**>), (<*>), (<*), (*>), (<|>), many, (<$))
 import Control.Monad (void, ap)
 import Data.Char (isLetter, isDigit)
-import Data.Vector as V
+import qualified Data.Vector as V
 import Data.Char(digitToInt)
+import Data.List(delete, union)
 
 --import Control.Applicative
 
@@ -33,7 +34,7 @@ expr :: Parser Expr
 expr =
         lambda
         <|>  (
-           ((,) <$> (variable <* ws) <*> optionMaybe expr) >>= ( \foo ->
+           ((,) <$> (variable <* ws) <*> optionMaybe expr') >>= ( \foo ->
               case foo of
                  (e1, Just e2) -> return $ App e1 e2
                  (e1, _) -> return $ e1
@@ -50,10 +51,10 @@ expr =
 -}
 
 app :: Parser Expr
-app =  App <$> (expr <* ws') <*> (expr)
+app =  App <$> (expr' <* ws') <*> (expr')
 
 lambda :: Parser Expr
-lambda =  Lambda <$> (  lambdaLit *> ws *> letter <* ws <* char '.' <* ws) <*> (expr <* ws )
+lambda =  Lambda <$> (  lambdaLit *> ws *> letter <* ws <* char '.' <* ws) <*> (expr' <* ws )
 
 lambdaLit :: Parser Char
 lambdaLit = oneOf ['\\', 'λ', '+']
@@ -78,3 +79,39 @@ ws' = void $ many1 $ oneOf " \n\t"
 
 
 ------------------- reduction -------------------------
+
+-- FIXME: set
+freeV :: Expr -> [Char]
+freeV (Var x) = [x]
+freeV (Lambda x t) = delete x $ freeV t
+freeV (App e1 e2) = union (freeV e1) (freeV e2)
+
+
+alpha:: Char -> Char -> Expr -> Expr
+alpha x z (Var y) | x == y    = Var z
+                  | otherwise =  Var y
+alpha x z (App a b) = App (alpha x z a) (alpha x z b)
+alpha x z (Lambda y f) | x == y = Lambda y f -- ???
+                       | otherwise = if elem y (freeV f)
+                                     then
+                                       error "y in FV (f)"
+                                     else
+                                      Lambda y (alpha x z f)
+
+subst :: Char -> Expr -> Expr -> Expr
+subst x e' v@(Var y) | x == y    = e'
+                     | otherwise = v
+subst x e' (App a b)  = App (subst x e' a) (subst x e' b)
+subst x e' l@(Lambda y f) | x == y = l
+                          | otherwise = if elem y (freeV e')
+                                        then
+                                          error "y in FV (f)" -- FIXME: replace
+                                        else
+                                          Lambda y (subst x e' f)
+
+
+beta :: Expr -> Expr
+beta (App (Lambda v e) e') = subst v e' e -- FIXME: do alpha if needed
+beta x = x
+
+-------------------- write interpreter --------------------------------
