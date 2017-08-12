252acd5d573ca5644f93521019add3be81cb874a
PanAeon <vitalii.voloshyn@gmail.com>
2017-08-12 14:39:30 +0300

    It only compiles every 2 tries... good luck.

--- src/RestDataLoader/Lambda.hs
+++ src/RestDataLoader/Lambda.hs
@@ -6,14 +6,14 @@ import Text.Parsec.Token(lexeme)
 import Text.Parsec.String (Parser)
 import Text.Parsec.Prim (parse, try, (<?>))
 import Text.Parsec.Char (oneOf, char, digit, letter, satisfy)
-import Text.Parsec.Combinator (many1, chainl1, between, eof, optionMaybe, notFollowedBy, anyToken)
+import Text.Parsec.Combinator (many1, chainl1, between, eof, optionMaybe,sepBy, notFollowedBy, anyToken)
 import Control.Applicative ((<$>), (<**>), (<*>), (<*), (*>), (<|>), many, (<$))
 import Control.Monad (void, ap)
 import Data.Char (isLetter, isDigit)
 import qualified Data.Vector as V
 import Data.Char(digitToInt)
 import Data.List(delete, union, find)
-
+import Debug.Trace(trace, traceShow, traceShowId)
 --import Control.Applicative
 
 data Variable = Variable Char
@@ -42,12 +42,15 @@ pexpr' = parens expr'
 
 
 
+explodeApp :: [Expr] -> Expr
+explodeApp = foldl1 (\a -> \r -> App a r)
+
 apply :: Parser Expr
 apply =
-   ((,) <$> (expr <* ws) <*> optionMaybe expr) >>= ( \foo ->
+   ((,) <$> (expr <* ws) <*> (expr `sepBy` ws) ) >>= ( \foo ->
       case foo of
-         (e1, Just e2) -> return $ App e1 e2
-         (e1, _) -> return $ e1
+         (e1, []) -> return $ e1
+         (e1, xs) -> return $ (explodeApp $ e1:xs)
    )
 
 
@@ -127,39 +130,77 @@ alpha x z (Lambda y f) | x == y = Lambda y f -- ???
                                       Lambda y (alpha x z f)
 
 subst :: Char -> Expr -> Expr -> Expr
-subst x e' v@(Var y) | x == y    = e'
+subst c e2 v@(Var y) | c == y    = e2
                      | otherwise = v
-subst x e' (App a b)  = App (subst x e' a) (subst x e' b)
-subst x e' l@(Lambda y f) | x == y = l
-                          | otherwise = if elem y (freeV e')
+subst c e2 (App a b)  = App (subst c e2 a) (subst c e2 b)
+subst c e2 l@(Lambda y f) | c == y = l
+                          | otherwise = if elem y (freeV e2)
                                         then
                                           error "y in FV (f)" -- FIXME: replace
                                         else
-                                          Lambda y (subst x e' f)
+                                          Lambda y (subst c e2 f)
 
 
 needsAlpha :: Expr -> Expr -> Bool
-needsAlpha e' (Lambda y _) = elem y (freeV e')
+needsAlpha e2 (Lambda y _) = elem (y) (freeV e2)
 needsAlpha _ _             = False
 
-fixFreeVars :: Char -> Expr -> (Char, Expr)
-fixFreeVars x e = (s, alpha x s e)
+fixFreeVars :: Expr -> Expr -> (Char, Expr)
+fixFreeVars e2 (Lambda v e1) = (s, alpha v s e1)
    where
      symbols = ['a'..'z']
-     fv      = freeV e
+     fv      = traceShowId $ freeV e2
      s       = maybe (error "not enough vars!") id $ find (\z -> not $ elem z fv) symbols
 
 
+isLambda :: Expr -> Bool
+isLambda (Lambda _ _) = True
+isLambda _ = False
+
+-- http://www.cs.cornell.edu/courses/cs6110/2014sp/Handouts/Sestoft.pdf
+
+-- same as subst, but will invoke alpha if needed
+
+subst' :: Expr -> Expr -> Expr
+subst' l@(Lambda v e1) e2 = if traceShowId $ needsAlpha e2 l then
+                              let (v', e1') = fixFreeVars e2 l
+                              in subst v' e2 e1'
+                            else
+                              trace "woot?" $ subst v e2 e1
+
+-- call-by-name, computes weak head normal form
+cbn :: Expr -> Expr
+cbn v@(Var _) = v
+cbn l@(Lambda _ _) = l
+cbn (App e1 e2) = case cbn e1 of
+                    l@(Lambda x e) -> cbn $ subst' l e2
+                    e1'          -> App e1' e2
+
+
+-- normal order reduction
 beta :: Expr -> Expr
-beta (App l@(Lambda v e) e') = if needsAlpha e' l then
-                                  let (v', efixed) = fixFreeVars v e
-                                  in subst v' e' efixed
-                               else
-                                 subst v e' e -- FIXME: do alpha if needed
-beta x = x
+beta (App e1 e2) = case cbn e1 of
+                    l@(Lambda x e) -> beta $ subst' l e2
+                    e1'            -> let e1'' = beta e1'
+                                      in App e1'' (beta e2)
+
+beta v@(Var _) = v
+beta (Lambda v e) = Lambda v $ beta e
+
 
 ------------ FIXME: beta, consecutive apply ----------------------------
 
+
+pprint :: Expr -> String
+pprint (Var a) = [a]
+pprint (App a b) =   pprint a ++ " " ++ "(" ++ pprint b ++")"
+pprint (Lambda a b) = "\\" ++ [a] ++ "." ++ pprint b
+
+test0 = "\\f.\\x.f ((\\n.\\f.\\x.f (n f x)) (\\f.\\x.x) f x)"
+test0Expr = Lambda 'f' (Lambda 'x' (App (Var 'f') (App (App (App (Lambda 'n' (Lambda 'f' (Lambda 'x' (App (Var 'f') (App (App (Var 'n') (Var 'f')) (Var 'x')))))) (Lambda 'f' (Lambda 'x' (Var 'x')))) (Var 'f')) (Var 'x'))))
+
+test1 = "(\\a.\\b.a (b (\\n.\\f.\\x.f (n f x))) (\\f.\\x.x)) (\\f.\\x.f (f (f x))) (\\f.\\x.f (f x))"
+test1E = App (App (Lambda 'a' (Lambda 'b' (App (App (Var 'a') (App (Var 'b') (Lambda 'n' (Lambda 'f' (Lambda 'x' (App (Var 'f') (App (App (Var 'n') (Var 'f')) (Var 'x')))))))) (Lambda 'f' (Lambda 'x' (Var 'x')))))) (Lambda 'f' (Lambda 'x' (App (Var 'f') (App (Var 'f') (App (Var 'f') (Var 'x'))))))) (Lambda 'f' (Lambda 'x' (App (Var 'f') (App (Var 'f') (Var 'x')))))
 -------------------- write interpreter --------------------------------
 -- "(((\\x y.(y x))(((((\\x y. (y x))(((\\x.x) 12)))) (\\x.x))))(\\x.x))"
 -- "((\\f.\\x.f (f (f x))) ((\\f.\\x.f (f x)) (\\n.\\f.\\x.f ((n f) x)))) (\\f.\\x.x)"
